6ec53279211443e3c25a4b37035e1c4ac69756e4
VK <viswanath.kaushik@gmail.com>
2020-05-20 15:28:30 -0500

    Fixes for script.js
    Fixed email and passwrod checks
    replaced res. send with res.json

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-const express   = require('express');
-const app       = express();
-const bcrypt    = require('bcrypt-nodejs');
-var bodyParser  = require('body-parser')
-var cors        = require('cors');
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt-nodejs');
+var bodyParser = require('body-parser')
+var cors = require('cors');
 const database = {
-  users: [{
-    id          : '123',
-    name        : 'Andrei',
-    email       : 'john@gmail.com',
-    entries     : 0,
-    joined      : new Date()
-  }],
-  secrets: {
-    users_id    : '123',
-    hash        : ''
-  }
+    users: [{
+        id: '123',
+        name: 'Andrei',
+        email: 'john@gmail.com',
+        password: 'cookies',
+        entries: 0,
+        joined: new Date()
+    }],
+    secrets: {
+        users_id: '123',
+        hash: ''
+    }
 }
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.get('/', (req, res) => res.send(database.users))
 
 app.post('/signin', (req, res) => {
-  var a         = JSON.parse(req.body);
-  if (a.username === database.users[0].email && a.password === database.secrets.hash) {
-    res.send('signed in');
-  } else {
-    res.json('access denied');
-  }
+    // var a         = JSON.parse(req.body);
+    var a = req.body;
+    if (a.email === database.users[0].email && a.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.json('access denied');
+    }
 })
 
 app.post('/findface', (req, res) => {
-  database.users.forEach(user => {
-    if (user.email === req.body.email) {
-      user.entries++
-      res.json(user.entries)
-    }
-  });
-  res.json('nope')
+    database.users.forEach(user => {
+        if (user.email === req.body.email) {
+            user.entries++
+                res.json(user.entries)
+        }
+    });
+    res.json('nope')
 })
 
 app.post('/register', (req, res) => {
-    bcrypt.hash(req.body.password, null, null, function(err, hash){
+    bcrypt.hash(req.body.password, null, null, function(err, hash) {
         console.log(hash);
     })
-  database.users.push({
-    id          : req.body.id,
-    name        : req.body.name,
-    email       : req.body.email,
-    entries     : 0,
-    joined      : new Date()
-  })
-  res.json(database.users[database.users.length - 1])
+    database.users.push({
+        id: req.body.id,
+        name: req.body.name,
+        email: req.body.email,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1])
 })
 
 app.get('/profile/:userId', (req, res) => {
-  database.users.forEach(user => {
-    if (user.id === req.params.userId) {
-      return res.json(user);
-    }
-  })
-  // res.json('no user')
+    database.users.forEach(user => {
+            if (user.id === req.params.userId) {
+                return res.json(user);
+            }
+        })
+        // res.json('no user')
 })
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
